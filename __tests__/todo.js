03eb8fb90b7a877356d78c46aa40e198b5cc2c89
Subhadip09 <97467258+Subhadip09@users.noreply.github.com>
2022-11-20 09:25:08 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("TodoList Test Suite", () => {
   beforeAll(() => {
@@ -24,5 +24,33 @@ describe("TodoList Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Overdue items", () => {
+    add({
+      title: "test item overdue",
+      dueDate: new Date(
+        new Date().setDate(new Date().getDate() - 1)
+      ).toLocaleDateString("en-CA"),
+      completed: false,
+    });
+    expect(overdue().length).toBe(1);
+  });
+
+  // checking if the dueToday function is working
+  test("Due today items", () => {
+    // 1 from beforeAll and 1 from the adding test above
+    expect(dueToday().length).toBe(2);
+  });
+
+  // checking if the dueLater function is working
+  test("Due later items", () => {
+    add({
+      title: "test item due later",
+      dueDate: new Date(
+        new Date().setDate(new Date().getDate() + 1)
+      ).toLocaleDateString("en-CA"),
+      completed: false,
+    });
+    expect(dueLater().length).toBe(1);
   });
 });
